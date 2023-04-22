8ac9a6b095138a47da9ea9098a788b04889b32bb
Nicolas Jimenez <njimenez@unpaz.edu.ar>
2023-04-21 22:22:40 -0300

    Creada clase e implementado el método SendByEmail de Report

---
+++ Before/SRP/SRP/EmailSender.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class EmailSender
+    {
+        public void SendByEmail(string addressee, string subject, string body)
+        {
+            Console.WriteLine("Se envió email a " + addressee + ", con asunto " + subject + " y cuerpo " + body);
+        }
+    }
+}
