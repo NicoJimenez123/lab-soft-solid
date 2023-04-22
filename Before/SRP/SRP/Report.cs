d36049d6224920b75a144e6415df24866c8903d7
Nicolas Jimenez <njimenez@unpaz.edu.ar>
2023-04-21 22:22:58 -0300

    Quitado método SendByEmail

--- Before/SRP/SRP/Report.cs
+++ Before/SRP/SRP/Report.cs
@@ -19,10 +19,5 @@ namespace SRP
             Console.WriteLine("Se generó reporte anual");
             return "<Contenido del reporte anual>";
         }
-
-        public void SendByEmail(string addressee, string subject, string body)
-        {
-            Console.WriteLine("Se envió email a " + addressee + ", con asunto " + subject + " y cuerpo " + body);
-        }
     }
 }
